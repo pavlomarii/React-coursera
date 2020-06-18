a57fae4e1cc8e9e78b40770a45a8eaea0c28a77e
Pavlo Marii <pavlomarii@gmail.com>
2020-06-18 14:02:16 +0300

    Fetch from Server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,6 +5,8 @@ import { Card, CardImg, CardText, CardBody,
 import { Link } from 'react-router-dom';
 import {Control, LocalForm, Errors} from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import {baseUrl} from '../shared/baseUrl';
+
 
 function RenderComments({comments, addComment, dishId}) {
 	if(comments != null){
@@ -36,7 +38,7 @@ function RenderDish({dish}){
 	return(
 		<div className='col-12 col-md-5 m-1'>
 			<Card>
-				<CardImg width="100%" src={dish.image} alt={dish.name}/>
+				<CardImg width="100%" src={baseUrl + dish.image} alt={dish.name}/>
 				<CardBody>
 					<CardTitle>{dish.name}</CardTitle>
 					<CardText>{dish.description}</CardText>
